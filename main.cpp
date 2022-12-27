fe5b37278f21d1a893334eecf5ce6d79343d2199
Bryan H <bryanhui77@hotmail.com>
2022-12-26 16:34:32 -0800

    basic hello world program

--- main.cpp
+++ main.cpp
@@ -1,4 +1,15 @@
+#include <string> 
+#include <vector> 
+#include <iostream>
+#include <stdio.h>
 
+using namespace std;
+
+int main() {
+
+    cout << "hello world" << endl;
+    return 0;
+}
 
 
 
