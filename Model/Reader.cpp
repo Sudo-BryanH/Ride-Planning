48aaf61d96a7d1105df95dc8fe6f30cb9c28ea22
Bryan H <bryanhui77@hotmail.com>
2023-01-02 16:45:48 -0800

    implemented extractPassengers

--- Model/Reader.cpp
+++ Model/Reader.cpp
@@ -22,9 +22,44 @@ unordered_map<string, DNode *> Reader::getDmap()
 void Reader::extractDrivers()
 {
 
+
 }
 
 void Reader::extractPassengers()
 {
-    
+    unordered_map<string, Node*> pm;
+
+
+    array passengers = json::parse("passengers");
+
+    for(auto& a : passengers) {
+        Person p = Person(json::parse("name"), json::parse("phone"), json::parse("gender"), json::parse("group"), json::parse("canBus"));
+        Node * n =  new Node(p);
+
+        if (pm.find(json::parse("group")) == pm.end())
+        {
+            Node * sentinel = new Node();
+            sentinel->next = n;
+            n->prev = sentinel;
+            sentinel->prev = n;
+            n->next = sentinel;
+
+            pm.emplace(make_pair(json::parse("group"), sentinel));
+
+
+        } else {
+
+            Node * sentinel = pm.find(json::parse("group"))->second;
+            sentinel->next = n;
+            n->prev = sentinel;
+            sentinel->prev = n;
+            n->next = sentinel;
+
+        }
+
+        
+    }
+
+    pmap = pm;
+
 }
