8179d5f7e5ae8d1203a5367022663041496e7ae4
Bryan H <bryanhui77@hotmail.com>
2023-01-05 00:37:14 -0800

    continued implementation of planner class. working on sort fn

--- model/Planner.cpp
+++ model/Planner.cpp
@@ -5,9 +5,12 @@
 
 
 
-Planner::Planner(unordered_map<string, DNode * > & driverlist, unordered_map<string, Node * > & passengerlist, vector<string> & grouplist)
+Planner::Planner(unordered_map<string, DNode * > & driverlist, unordered_map<string, Node * > & passengerlist, vector<string> & grouplist) 
 {
-
+    pub = Publisher();
+    driverlist = driverlist;
+    passengerlist = passengerlist;
+    grouplist = grouplist;
 
 }
 
@@ -15,18 +18,56 @@ void Planner::planride()
 {
     for(string g : grouplist)
     {
-        if (driverlist.find(g) != driverlist.end() && passengerlist.find(g) == passengerlist.find(g))
-        {
+        auto dl = driverlist.find(g);
+        auto pl = passengerlist.find(g);
+        sort(dl, pl);
+    }
 
+    auto dl = driverlist.find("male");
+    auto pl = passengerlist.find("male");
+
+
+    auto dl = driverlist.find("female");
+    auto pl = passengerlist.find("female");
+
+
+    auto dl = driverlist.find("misc");
+    auto pl = passengerlist.find("misc");
+
+
+
+}
+
+void Planner::sort(unordered_map<string, DNode *>::iterator dl, unordered_map<string, Node *>::iterator pl)
+{
+
+    //reminder to use plist's regular add node fn
+
+        if (dl != driverlist.end() && pl == passengerlist.end())
+        {
+          
         }
-        else if (driverlist.find(g) == driverlist.end() && passengerlist.find(g) != passengerlist.find(g))
+        else if (dl == driverlist.end() && pl != passengerlist.end())
         {
-
+            
         }
         else
         {
+            Node * curp = pl->second;
+            DNode * curd = dl->second;
+
+            while(curd)
+            {
+                while(curd->getPerson().getCapacity() != 0 && curp)
+                {
+
+                }
+                curd = curd->next;
+            }
+
+
 
         }
-    }
+
 
 }
--- model/Planner.h
+++ model/Planner.h
@@ -28,14 +28,25 @@ class Planner
 
     //uses the two maps to assign passengers to drivers
     void planride();
+    
+    /*
+    Helper for planride. Based on capacity c of a driver, a plist will be used to group c passenger nodes and assign them to 
+    a driver. 
+    - After a driver is filled, it's assignment will be published
+    - If at the end, driver(s) are not filled, it will remain untouched
+    - if there are remaining passengers, or the passenger could bus, they will be moved to their respective genders or misc. 
+    */
+   void sort(unordered_map<string, DNode *>::iterator dl, unordered_map<string, Node *>::iterator pl);
 
+    
 
     // to iterate over an unordered map, use https://www.geeksforgeeks.org/set-cbegin-and-cend-function-in-c-stl/
     private: 
 
-    unordered_map<string, DNode> driverlist;
-    unordered_map<string, Node> passengerlist;
+    unordered_map<string, DNode *> driverlist;
+    unordered_map<string, Node *> passengerlist;
     vector<string> grouplist;
+    Publisher pub;
 
 
 };
